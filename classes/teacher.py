6e2974f2b9241c2a3e8c8e59fa018ed1f15b5f1a
FredPadgham007 <mr.f.padgham@gmail.com>
2023-09-24 14:30:21 +0100

    updated

--- classes/teacher.py
+++ classes/teacher.py
@@ -1,20 +1,6 @@
 from typing import List
 
-
-DEFAULT_DATA = {
-    "P1 ClassSet": ["", "", "", ""],
-    "P1 Room": [0, 0, 0, 0],
-    "P2 ClassSet": ["", "", "", ""],
-    "P2 Room": [0, 0, 0, 0],
-    "P3 ClassSet": ["", "", "", ""],
-    "P3 Room": [0, 0, 0, 0],
-    "P4 ClassSet": ["", "", "", ""],
-    "P4 Room": [0, 0, 0, 0],
-    "P5 ClassSet": ["", "", "", ""],
-    "P5 Room": [0, 0, 0, 0],
-    "P6 ClassSet": ["", "", "", ""],
-    "P6 Room": [0, 0, 0, 0],
-}
+from classes.timetable import Timetable
 
 
 class Teacher:
@@ -23,15 +9,15 @@ class Teacher:
         name: str,
         subjects: List[str],
         room: int,
-        available: bool = True,
+        timetable: Timetable = Timetable(),
     ) -> None:
         self.name = name
         self.subjects = subjects
         self.pref_room = room
-        self.available = available
+        self.timetable = timetable
 
     def __repr__(self) -> str:
-        return f"Teacher: name={self.name}, subjects={self.subjects}, pref_room={self.pref_room}, available={self.available}"
+        return f"Teacher: name={self.name}, subjects={self.subjects}, pref_room={self.pref_room} timetable=\n{self.timetable}"
 
     # Getters
 
@@ -43,9 +29,6 @@ class Teacher:
 
     def get_pref_room(self):
         return self.pref_room
-
-    def is_available(self):
-        return self.available
 
     # Setters
 
